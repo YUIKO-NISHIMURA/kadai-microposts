1df1381bfed6da86ed677f0b8f7728826c4b14cb
yuiko.nishimura <yuzuko.1020@gmail.com>
2021-09-23 13:10:22 +0000

    for model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :reverses_of_favorite, class_name:"Favorite"
+  has_many :favorite_users, through: :reverses_of_favorite, source: :user
+  
   validates :content, presence: true, length:{ maximum:255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationships, class_name: "Relationship", foreign_key: "follow_id"
   has_many :followers, through: :reverses_of_relationships, source: :user
+  
+  has_many :favorites
+  has_many :favorite_microposts, through: :favorites, source: :micropost
 
   def follow(other_user)
     unless self == other_user
@@ -25,9 +28,22 @@ class User < ApplicationRecord
   def following?(other_user)
     self.followings.include?(other_user)
   end 
-
+  
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end 
+  
+  def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end 
+  
+  def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite 
+  end 
+  
+  def favorite?(micropost)
+    self.favorite_microposts.include?(micropost)
   end 
 
 end
