77324d6a44de4cd36a9a3d2891b26d286ff811c8
yuiko.nishimura <yuzuko.1020@gmail.com>
2021-09-24 07:41:50 +0000

    for favorites

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
-  has_many :reverses_of_favorite, class_name:"Favorite"
-  has_many :favorite_users, through: :reverses_of_favorite, source: :user
+  has_many :reverses_of_favorite, class_name:"Favorite", dependent: :destroy
+  has_many :favorite_users, through: :reverses_of_favorite, source: :user, dependent: :destroy
   
   validates :content, presence: true, length:{ maximum:255 }
 end
